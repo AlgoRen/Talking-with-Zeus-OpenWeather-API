ce2592fe08246f8400cd619116c2c5827cc00d2d
Ren <algorenstudio@gmail.com>
2020-05-04 20:58:44 -0400

    Creating the results of the callback to display only one of the cities weather info on the page at a time. I still need to make the weather for the city to load for the button clicked. At the moment im able to console log the last buttons results.

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ function loadWeather() {
     console.log(search.main.humidity)
     console.log(search.wind.speed)
     
+
     var cityName = $("<h2>").text(search.name)
     $("#currentWeather").append(cityName);
     var currentTemp = $("<h3>").text("Tempature: " + Math.round(search.main.temp) + "F")
@@ -23,6 +24,8 @@ function loadWeather() {
     $("#currentWeather").append(currentHumidity);
     var currentWindSpeed = $("<h3>").text("Wind speed: " + Math.round(search.wind.speed) + "MPH")
     $("#currentWeather").append(currentWindSpeed);
+
+    
     long = search.coord.lon
     lat = search.coord.lat
     return long, lat;
@@ -30,12 +33,18 @@ function loadWeather() {
 
 
 function addButton() {
-    var cityButton = $("<Button>").text(this.search.name)
+    var cityButton = $("<Button>").text(this.search.name).attr("class", "cityButton")
     $("#buttonArea").append(cityButton)
+    
 }
 
-function loadResults() {
-
+function loadResults(e) {
+    // e.preventDefault()
+    // How can I get the callback from OpenWeather API from button click?
+    // Read the button value and put it into city 
+    var city = this.innerText
+    // Trying to figure out how to load weather results for the city that corresponds to the button clicked.
+    loadWeather();
 }
 
 function saveResults() {
@@ -43,9 +52,12 @@ function saveResults() {
 }
 
     
-// Getting input from search field
-$("#searchCity").on("click", function (e) {
-    e.preventDefault()
+// Getting input from search field and getting callback from OpenWeather API.
+$("#searchCity").on("click", weatherCall);
+$(document).on("click", ".cityButton", loadResults);
+
+function weatherCall(e) {
+    // e.preventDefault()
     // API CALL for Weather
     var APICall = "http://api.openweathermap.org/data/2.5/weather?q="
     var APIkey = "&APPID=11f39808453d026eda60eff244a8ef1e"
@@ -69,7 +81,9 @@ $("#searchCity").on("click", function (e) {
         addButton(this)
         savingCities.push(city);
         localStorage.setItem("cities", JSON.stringify(savingCities));
+        $("#currentWeather").empty();
         loadWeather();
+        
         console.log(savingCities)
         console.log("---------------------")
         console.log("Longitude: " + long)
@@ -86,7 +100,11 @@ $("#searchCity").on("click", function (e) {
             $("#currentWeather").append(UV);
         })
     })
+    
+    // Above solution is only running the loadResults function when the first button is clicked,
+    // im guessing its because the on click listener is only being attached the the first .cityButton
+    // item so I'll probably have to put the listener on the button container and filter for clicks on buttons only.
     // Adding buttons onto page.
     // addButton();
     // End of SearchCity on click function.
-});
+};
